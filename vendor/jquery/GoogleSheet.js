e414a906e0d8628f12440ff511a78ef2fb751c8a
panku121 <pankajbca12101@gmail.com>
2024-09-01 17:20:00 +0530

    Admission date Column added to Footer form and Popupform

--- vendor/jquery/GoogleSheet.js
+++ vendor/jquery/GoogleSheet.js
@@ -1,11 +1,13 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzzOCv3nl7Hi5vco7QmRDrshpZFkjpX0YMmuuGwlpL7Y764UovpOMKB0CIFePS7kh7s/exec';
-const form = document.forms['contact-form']
 
-form.addEventListener( 'submit', e=>{
-    console.log('hi')
-    e.preventDefault()
-    fetch( scriptURL, { method: 'POST', body: new FormData(form) } )
-    .then( response => alert( "Thank you! Your form is submitted successfully." ) )
-    .then( () => { window.location.reload(); } )
-    .catch( error => console.error( 'Error!', error.message ) )
-} )
+function sendDataToGsheet() {
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbzzOCv3nl7Hi5vco7QmRDrshpZFkjpX0YMmuuGwlpL7Y764UovpOMKB0CIFePS7kh7s/exec';
+    const form = document.forms['contact-form'];
+    
+    form.addEventListener( 'submit', e=>{
+        e.preventDefault()
+        fetch( scriptURL, { method: 'POST', body: new FormData(form) } )
+        .then( response => alert( "Thank you! Your form is submitted successfully." ) )
+        .then( () => { window.location.reload(); } )
+        .catch( error => console.error( 'Error!', error.message ) )
+    } )
+}
